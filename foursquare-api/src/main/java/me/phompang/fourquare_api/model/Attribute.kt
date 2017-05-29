c51c72e937f047246a0c62a86d4fe4312663bc9f
PhompAng <tingtong003tomy@gmail.com>
2017-05-30 00:51:09 +0700

    update venue model

---
+++ foursquare-api/src/main/java/me/phompang/fourquare_api/model/Attribute.kt
@@ -0,0 +1,8 @@
+package me.phompang.fourquare_api.model
+
+/**
+ * Created by phompang on 5/30/2017 AD.
+ */
+data class Attribute(val displayName: String,
+                     val displayValue: String,
+                     val priceTier: Integer)
---
+++ foursquare-api/src/main/java/me/phompang/fourquare_api/model/Reason.kt
@@ -0,0 +1,8 @@
+package me.phompang.fourquare_api.model
+
+/**
+ * Created by phompang on 5/30/2017 AD.
+ */
+data class Reason(val summary: String,
+                  val type: String,
+                  val reasonName: String)
